02c83c408af58f2f2796381ee09e9f6995d1f4e3
crissilvaf7 <cris.silvaf7@gmail.com>
2022-11-18 21:14:22 -0700

    Test

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ import { OutlinePass } from 'https://unpkg.com/three@0.143.0/examples/jsm/postpr
 import { FXAAShader } from 'https://unpkg.com/three@0.143.0/examples/jsm/shaders/FXAAShader.js';*/
 //===== XR =====\\
 import { VRButton } from 'https://unpkg.com/three@0.143.0/examples/jsm/webxr/VRButton.js';
-import { ARButton } from 'https://unpkg.com/three@0.143.0/examples/jsm/webxr/ARButton.js';
 import { XRControllerModelFactory } from 'https://unpkg.com/three@0.143.0/examples/jsm/webxr/XRControllerModelFactory.js';
 import { OculusHandModel } from './OculusHandModel.js';
 import { OculusHandPointerModel } from 'https://unpkg.com/three@0.143.0/examples/jsm/webxr/OculusHandPointerModel.js';
@@ -93,7 +92,7 @@ function init() {
     viewer.appendChild(renderer.domElement);
     renderer.xr.enabled = true;
     renderer.xr.cameraAutoUpdate = false;
-    renderer.xr.setReferenceSpaceType("viewer");
+    renderer.xr.setReferenceSpace(XRReferenceSpace.viewer);
     console.log(renderer.xr);
     //
     let vrButton = VRButton.createButton(renderer);
